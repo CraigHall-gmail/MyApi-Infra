6fe77770f3e1b10baeecf3f1b98332cfb1c5bba3
HallCraig <craigh@tfg.co.za>
2026-05-19 20:20:28 +0200

    Fix

--- modules/key-vault/main.tf
+++ modules/key-vault/main.tf
@@ -12,7 +12,7 @@ resource "azurerm_key_vault" "this" {
   location                   = var.location
   tenant_id                  = data.azurerm_client_config.current.tenant_id
   sku_name                   = "standard"
-  rbac_authorization_enabled = true
+  enable_rbac_authorization  = true
   soft_delete_retention_days = 90
   purge_protection_enabled   = true
   tags                       = var.tags
